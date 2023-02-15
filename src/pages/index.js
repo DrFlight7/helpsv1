f37159dd8f6295916433166e98cc63dbf3140793
Marco Rado <drflight7@gmail.com>
2023-02-15 08:48:12 +0800

    img changed to Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import {
   updateDoc,
   deleteDoc
 } from 'firebase/firestore';
+import Image from 'next/image';
 
 export default function Home() {
 
@@ -110,7 +111,7 @@ export default function Home() {
       <div
         className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
       >
-        <img
+        <Image
           /*src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"*/
           src="https://liceo.edu.ph/images/ico-liceo-big.png"
           className="w-full print:hidden"
